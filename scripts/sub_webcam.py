6600a4a074d5afb13eb087fa40572597b1e6eeae
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2021-01-29 23:59:12 +0530

    Add files via upload

---
+++ scripts/sub_webcam.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3 
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge 
+import cv2
+
+def callback(data):
+	br = CvBridge()
+
+	rospy.loginfo("receiving video frame")
+	current_frame = br.imgmssg_to_cv2(data)
+
+	cv2.imshow("camera", current_frame)
+
+	cv2.waitKey(1)
+
+def receive_message():
+	rospy.init_node('video_sub_py', anonymous=True)
+
+	rospy.Subscriber('video_frames', Image, callback)
+	rospy.spin()
+
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	receive_message()
