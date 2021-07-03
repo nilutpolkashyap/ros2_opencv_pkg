207f4833eac7615bebf36f04da3bc0b9aafbf01d
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-07-03 23:58:00 +0530

    commit

---
+++ scripts/video_read.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy 
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+def callback(data):
+	bridge = CvBridge()
+	rospy.loginfo("Starting Video Frames")
+
+	current_img  = bridge.imgmsg_to_cv2(data)
+
+	cv2.imshow("video", current_img)
+	cv2.waitKey(1)
+
+def video_subscriber():
+	rospy.init_node("video_subscriber_py", anonymous=True)
+
+	rospy.Subscriber("/camera/image_raw", Image, callback)
+
+	rospy.spin()
+
+	cv2.destroyAllWindows()
+
+if __name__ ==  "__main__":
+	video_subscriber()
