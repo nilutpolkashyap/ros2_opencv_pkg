ecabcb824a03994267b0250f56a949e471c4ae66
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-07-04 23:57:06 +0530

    updated package

--- scripts/video_read.py
+++ scripts/video_read.py
@@ -7,15 +7,17 @@ from cv_bridge import CvBridge
 
 def callback(data):
 	bridge = CvBridge()
-	rospy.loginfo("Starting Video Frames")
 
-	current_img  = bridge.imgmsg_to_cv2(data)
+	current_img  = bridge.imgmsg_to_cv2(data,desired_encoding='bgra8')
 
-	cv2.imshow("video", current_img)
+	resized = cv2.resize(current_img, (0, 0), fx = 0.6, fy = 0.6)
+
+	cv2.imshow("video", resized)
 	cv2.waitKey(1)
 
 def video_subscriber():
 	rospy.init_node("video_subscriber_py", anonymous=True)
+	rospy.loginfo("Starting Video Frames")
 
 	rospy.Subscriber("/camera/image_raw", Image, callback)
 
